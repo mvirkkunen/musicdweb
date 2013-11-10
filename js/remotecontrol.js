782e4c63e07233fa9286c62b3d2b1ab6ffc20999
Matti Virkkunen <mvirkkunen@gmail.com>
2013-11-10 19:13:25 +0200

    Send current track information to remote control

--- js/remotecontrol.js
+++ js/remotecontrol.js
@@ -7,6 +7,7 @@ var REMOTE_URL = "http://localhost:48278/";
 musicd.RemoteControl = function(player) {
     this._player = player;
     this._player.state.subscribe(this._updateState, this);
+    this._player.track.subscribe(this._updateTrack, this);
 };
 
 musicd.RemoteControl.prototype = {
@@ -16,9 +17,9 @@ musicd.RemoteControl.prototype = {
 
         this.eventSource = new EventSource(REMOTE_URL + "events");
         this.eventSource.addEventListener("command", this._command.bind(this));
-        this.eventSource.onopen = this._updateState.bind(this);
+        this.eventSource.onopen = this._updateAll.bind(this);
 
-        this._updateState();
+        this._updateAll();
     },
 
     disable: function() {
@@ -28,14 +29,43 @@ musicd.RemoteControl.prototype = {
         }
     },
 
+    _updateAll: function() {
+        this._updateState();
+        this._updateTrack();
+    },
+
     _updateState: function() {
-        if (this.eventSource)
-            $.post(REMOTE_URL + "state/" + musicd.PlayerState[this._player.state()].toLowerCase());
+        if (!this.eventSource)
+            return;
+
+        $.post(REMOTE_URL + "state/" + musicd.PlayerState[this._player.state()].toLowerCase());
+    },
+
+    _updateTrack: function() {
+        if (!this.eventSource)
+            return;
+
+        var track = this._player.track(),
+            url = REMOTE_URL + "track";
+
+        if (track) {
+            url += "?id=" + track.id
+                + "&title=" + encodeURIComponent(track.title)
+                + "&artist=" + encodeURIComponent(track.artist)
+                + "&album=" + encodeURIComponent(track.album)
+                + "&duration=" + encodeURIComponent(track.duration);
+        } else {
+            url += "?id=0";
+        }
+
+        $.post(url);
     },
 
     _command: function(e) {
-        if (this.eventSource)
-            this._player.execCommand(e.data);
+        if (!this.eventSource)
+            return;
+
+        this._player.execCommand(e.data);
     }
 };
 
